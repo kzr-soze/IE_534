966d4d44a16f651ba736874d3d38399315efa118
Murray <tsmurra2@illinois.edu>
2019-11-08 14:12:03 -0600

    Starting HW7, part 1a

--- HW7/1b.py
+++ HW7/1b.py
@@ -11,6 +11,26 @@ from torch.autograd import Variable
 from Discriminator import Discriminator
 from Generator import Generator
 
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import os
+
+def plot(samples):
+    fig = plt.figure(figsize=(10, 10))
+    gs = gridspec.GridSpec(10, 10)
+    gs.update(wspace=0.02, hspace=0.02)
+
+    for i, sample in enumerate(samples):
+        ax = plt.subplot(gs[i])
+        plt.axis('off')
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        ax.set_aspect('equal')
+        plt.imshow(sample)
+    return fig
+
 def calc_gradient_penalty(netD, real_data, fake_data):
     DIM = 32
     LAMBDA = 10
@@ -88,11 +108,24 @@ n_z = 100
 # criterion = nn.CrossEntropyLoss()
 # optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
-total_epochs = 100
+total_epochs = 200
+gen_train = 1
+
 
 train_loss = []
 train_accu = []
 test_accu = []
+
+np.random.seed(352)
+label = np.asarray(list(range(10))*10)
+noise = np.random.normal(0,1,(100,n_z))
+label_onehot = np.zeros((100,n_classes))
+label_onehot[np.arange(100), label] = 1
+noise[np.arange(100), :n_classes] = label_onehot[np.arange(100)]
+noise = noise.astype(np.float32)
+
+save_noise = torch.from_numpy(noise)
+save_noise = Variable(save_noise).cuda()
 
 print("Beginning Training...")
 
@@ -104,7 +137,15 @@ for epoch in range(total_epochs):  # loop over the dataset multiple times
         for param_group in optimizer.param_groups:
             param_group['lr'] = learning_rate/100.0
 
-    model.train()
+    loss1 = []
+    loss2 = []
+    loss3 = []
+    loss4 = []
+    loss5 = []
+    acc1 = []
+
+    aG.train()
+    aD.train()
     epoch_acc = 0.0
     epoch_loss = 0.0
 
@@ -117,21 +158,91 @@ for epoch in range(total_epochs):  # loop over the dataset multiple times
         if(Y_train_batch.shape[0] < batch_size):
             continue
 
-        X_train_batch = Variable(X_train_batch).cuda()
-        Y_train_batch = Variable(Y_train_batch).cuda()
-        _, output = model(X_train_batch)
+        # train G
+        if((batch_idx%gen_train)==0):
+            for p in aD.parameters():
+                p.requires_grad_(False)
 
-        loss = criterion(output, Y_train_batch)
-        optimizer.zero_grad()
+            aG.zero_grad()
 
-        loss.backward()
-        for group in optimizer.param_groups:
-            for p in group['params']:
-                state = optimizer.state[p]
-                if 'step' in state.keys():
-                    if state['step'] >= 1024:
-                        state['step'] = 1000
-        optimizer.step()
+            label = np.random.randint(0,n_classes,batch_size)
+            noise = np.random.normal(0,1,(batch_size,n_z))
+            label_onehot = np.zeros((batch_size,n_classes))
+            label_onehot[np.arange(batch_size), label] = 1
+            noise[np.arange(batch_size), :n_classes] = label_onehot[np.arange(batch_size)]
+            noise = noise.astype(np.float32)
+            noise = torch.from_numpy(noise)
+            noise = Variable(noise).cuda()
+            fake_label = Variable(torch.from_numpy(label)).cuda()
+
+            fake_data = aG(noise)
+            gen_source, gen_class  = aD(fake_data)
+
+            gen_source = gen_source.mean()
+            gen_class = criterion(gen_class, fake_label)
+
+            gen_cost = -gen_source + gen_class
+            gen_cost.backward()
+
+            optimizer_g.step()
+
+        # train D
+        for p in aD.parameters():
+            p.requires_grad_(True)
+
+        aD.zero_grad()
+
+        # train discriminator with input from generator
+        label = np.random.randint(0,n_classes,batch_size)
+        noise = np.random.normal(0,1,(batch_size,n_z))
+        label_onehot = np.zeros((batch_size,n_classes))
+        label_onehot[np.arange(batch_size), label] = 1
+        noise[np.arange(batch_size), :n_classes] = label_onehot[np.arange(batch_size)]
+        noise = noise.astype(np.float32)
+        noise = torch.from_numpy(noise)
+        noise = Variable(noise).cuda()
+        fake_label = Variable(torch.from_numpy(label)).cuda()
+        with torch.no_grad():
+            fake_data = aG(noise)
+
+        disc_fake_source, disc_fake_class = aD(fake_data)
+
+        disc_fake_source = disc_fake_source.mean()
+        disc_fake_class = criterion(disc_fake_class, fake_label)
+
+        # train discriminator with input from the discriminator
+        real_data = Variable(X_train_batch).cuda()
+        real_label = Variable(Y_train_batch).cuda()
+
+        disc_real_source, disc_real_class = aD(real_data)
+
+        prediction = disc_real_class.data.max(1)[1]
+        accuracy = ( float( prediction.eq(real_label.data).sum() ) /float(batch_size))*100.0
+
+        disc_real_source = disc_real_source.mean()
+        disc_real_class = criterion(disc_real_class, real_label)
+
+        gradient_penalty = calc_gradient_penalty(aD,real_data,fake_data)
+
+        disc_cost = disc_fake_source - disc_real_source + disc_real_class + disc_fake_class + gradient_penalty
+        disc_cost.backward()
+
+        optimizer_d.step()
+
+        # within the training loop
+        loss1.append(gradient_penalty.item())
+        loss2.append(disc_fake_source.item())
+        loss3.append(disc_real_source.item())
+        loss4.append(disc_real_class.item())
+        loss5.append(disc_fake_class.item())
+        acc1.append(accuracy)
+        if((batch_idx%50)==0):
+            print(epoch, batch_idx, "%.2f" % np.mean(loss1),
+                                    "%.2f" % np.mean(loss2),
+                                    "%.2f" % np.mean(loss3),
+                                    "%.2f" % np.mean(loss4),
+                                    "%.2f" % np.mean(loss5),
+                                    "%.2f" % np.mean(acc1))
 
         # print statistics
         # running_loss += loss.item()
@@ -139,84 +250,61 @@ for epoch in range(total_epochs):  # loop over the dataset multiple times
         #     print('[%d, %5d] loss: %.3f' %
         #           (epoch + 1, batch_idx + 1, running_loss / (50*batch_size)))
         #     running_loss = 0.0
-        epoch_counter += batch_size
-        epoch_loss +=loss.data[0]
-        _,predicted = torch.max(output.data,1)
-        epoch_acc += (predicted == Y_train_batch).sum().item()
+        # epoch_counter += batch_size
+        # epoch_loss +=loss.data[0]
+        # _,predicted = torch.max(output.data,1)
+        # epoch_acc += (predicted == Y_train_batch).sum().item()
 
     # print(epoch_counter,epoch_acc)
-    epoch_acc /= epoch_counter
-    epoch_loss /= (epoch_counter/batch_size)
-
-    train_loss.append(epoch_loss)
-    train_accu.append(epoch_acc)
-
-    print(epoch, "%.2f" % (epoch_acc*100.0), "%.4f" % epoch_loss, "%.4f" % float(time.time()-time1))
+    # epoch_acc /= epoch_counter
+    # epoch_loss /= (epoch_counter/batch_size)
+    #
+    # train_loss.append(epoch_loss)
+    # train_accu.append(epoch_acc)
+    #
+    # print(epoch, "%.2f" % (epoch_acc*100.0), "%.4f" % epoch_loss, "%.4f" % float(time.time()-time1))
 
     # Begin testing accuracy
-    model.eval()
 
-    epoch_acc = 0.0
-    epoch_loss = 0.0
+    # Test the model
+    aD.eval()
+    with torch.no_grad():
+        test_accu = []
+        for batch_idx, (X_test_batch, Y_test_batch) in enumerate(testloader):
+            X_test_batch, Y_test_batch= Variable(X_test_batch).cuda(),Variable(Y_test_batch).cuda()
 
-    epoch_counter = 0
+            with torch.no_grad():
+                _, output = aD(X_test_batch)
 
-    time1 = time.time()
+            prediction = output.data.max(1)[1] # first column has actual prob.
+            accuracy = ( float( prediction.eq(Y_test_batch.data).sum() ) /float(batch_size))*100.0
+            test_accu.append(accuracy)
+            accuracy_test = np.mean(test_accu)
+    print('Testing',accuracy_test, time.time()-start_time)
 
-    running_loss = 0.0
-    # with torch.no_grad():
-    for batch_idx, (X_test_batch, Y_test_batch) in enumerate(testloader):
+    ### save output
+    with torch.no_grad():
+        aG.eval()
+        samples = aG(save_noise)
+        samples = samples.data.cpu().numpy()
+        samples += 1.0
+        samples /= 2.0
+        samples = samples.transpose(0,2,3,1)
+        aG.train()
 
-        if(Y_test_batch.shape[0] < batch_size):
-            continue
+    fig = plot(samples)
+    plt.savefig('output/%s.png' % str(epoch).zfill(3), bbox_inches='tight')
+    plt.close(fig)
 
-        X_test_batch = Variable(X_test_batch).cuda()
-        Y_test_batch = Variable(Y_test_batch).cuda()
-        _, output = model(X_test_batch)
+    if(((epoch+1)%1)==0):
+        torch.save(aG,'tempG.model')
+        torch.save(aD,'tempD.model')
 
-        loss = criterion(output, Y_test_batch)
-        optimizer.zero_grad()
-
-        loss.backward()
-        optimizer.step()
-
-        # print statistics
-        # running_loss += loss.item()
-        # if batch_idx % 50 == 49:    # print every 50 mini-batches
-        #     print('[%d, %5d] loss: %.3f' %
-        #           (epoch + 1, batch_idx + 1, running_loss / 50))
-        #     running_loss = 0.0
-        epoch_counter += batch_size
-        epoch_loss +=loss.data[0]
-        _,predicted = torch.max(output.data,1)
-        epoch_acc += (predicted == Y_test_batch).sum().item()
-
-    # print(epoch_counter,epoch_acc)
-    epoch_acc /= epoch_counter
-    epoch_loss /= (epoch_counter/batch_size)
-
-    train_loss.append(epoch_loss)
-    train_accu.append(epoch_acc)
-
-    print(epoch, "%.2f" % (epoch_acc*100.0), "%.4f" % epoch_loss, "%.4f" % float(time.time()-time1))
-    torch.save(model,'prelim_cifar10.model')
+    # print(epoch, "%.2f" % (epoch_acc*100.0), "%.4f" % epoch_loss, "%.4f" % float(time.time()-time1))
+    # torch.save(model,'prelim_cifar10.model')
 
 
 print('Finished Training')
 
-torch.save(model,'cifar10.model')
-
-# Evaluate accuracy
-
-# correct = 0
-# total = 0
-# with torch.no_grad():
-#     for data in testloader:
-#         images, labels = data
-#         outputs = net(images)
-#         _, predicted = torch.max(outputs.data, 1)
-#         total += labels.size(0)
-#         correct += (predicted == labels).sum().item()
-#
-# print('Accuracy of the network on the 10000 test images: %d %%' % (
-#     100 * correct / total))
+torch.save(aG,'generator.model')
+torch.save(aD,'discriminator.model')
