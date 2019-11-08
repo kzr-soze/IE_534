b077935c83d1aec18fbbf777d68b9a2e7a054ce3
Murray <tsmurra2@illinois.edu>
2019-11-08 12:39:38 -0600

    Starting HW7, part 1a

--- HW7/1a.py
+++ HW7/1a.py
@@ -104,6 +104,7 @@ for epoch in range(total_epochs):  # loop over the dataset multiple times
         _,predicted = torch.max(output.data,1)
         epoch_acc += (predicted == Y_train_batch)
 
+    print(epoch_counter,epoch_acc)
     epoch_acc /= epoch_counter
     epoch_loss /= (epoch_counter/batch_size)
 
