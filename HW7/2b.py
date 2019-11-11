c1e06e5a2bfac0d6862fd6b3d1edbf356697eefa
Murray <tsmurra2@illinois.edu>
2019-11-11 13:22:43 -0600

    Starting HW7, part 2a.

--- HW7/2b.py
+++ HW7/2b.py
@@ -67,54 +67,66 @@ model.eval()
 
 batch_idx, (X_batch, Y_batch) = testloader.__next__()
 X_batch = Variable(X_batch,requires_grad=True).cuda()
-Y_batch_alternate = (Y_batch + 1)%10
-Y_batch_alternate = Variable(Y_batch_alternate).cuda()
-Y_batch = Variable(Y_batch).cuda()
+X = X_batch.mean(dim=0)
+X = X.repeat(10,1,1,1)
 
-_, output = model(X_batch)
-prediction = output.data.max(1)[1] # first column has actual prob.
-accuracy = ( float( prediction.eq(Y_batch.data).sum() ) /float(batch_size))*100.0
-print(accuracy)
+Y = torch.arange(10).type(torch.int64)
+Y = Variable(Y).cuda()
 
-## slightly jitter all input images
-criterion = nn.CrossEntropyLoss(reduce=False)
-loss = criterion(output, Y_batch_alternate)
+lr = 0.1
+weight_decay = 0.001
+for i in xrange(200):
+    _, output = model(X)
 
-gradients = torch.autograd.grad(outputs=loss, inputs=X_batch,
-                          grad_outputs=torch.ones(loss.size()).cuda(),
-                          create_graph=True, retain_graph=False, only_inputs=True)[0]
+    loss = -output[torch.arange(10).type(torch.int64),torch.arange(10).type(torch.int64)]
+    gradients = torch.autograd.grad(outputs=loss, inputs=X,
+                              grad_outputs=torch.ones(loss.size()).cuda(),
+                              create_graph=True, retain_graph=False, only_inputs=True)[0]
 
-# save gradient jitter
-gradient_image = gradients.data.cpu().numpy()
-gradient_image = (gradient_image - np.min(gradient_image))/(np.max(gradient_image)-np.min(gradient_image))
-gradient_image = gradient_image.transpose(0,2,3,1)
-fig = plot(gradient_image[0:100])
-plt.savefig('visualization/gradient_image.png', bbox_inches='tight')
-plt.close(fig)
+    prediction = output.data.max(1)[1] # first column has actual prob.
+    accuracy = ( float( prediction.eq(Y.data).sum() ) /float(10.0))*100.0
+    print(i,accuracy,-loss)
 
-# jitter input image
-gradients[gradients>0.0] = 1.0
-gradients[gradients<0.0] = -1.0
+    X = X - lr*gradients.data - weight_decay*X.data*torch.abs(X.data)
+    X[X>1.0] = 1.0
+    X[X<-1.0] = -1.0
 
-gain = 8.0
-X_batch_modified = X_batch - gain*0.007843137*gradients
-X_batch_modified[X_batch_modified>1.0] = 1.0
-X_batch_modified[X_batch_modified<-1.0] = -1.0
-
-## evaluate new fake images
-_, output = model(X_batch_modified)
-prediction = output.data.max(1)[1] # first column has actual prob.
-accuracy = ( float( prediction.eq(Y_batch.data).sum() ) /float(batch_size))*100.0
-print(accuracy)
-
-## save fake images
-samples = X_batch_modified.data.cpu().numpy()
+## save new images
+samples = X.data.cpu().numpy()
 samples += 1.0
 samples /= 2.0
 samples = samples.transpose(0,2,3,1)
 
-fig = plot(samples[0:100])
-plt.savefig('visualization/jittered_images.png', bbox_inches='tight')
+fig = plot(samples)
+plt.savefig('visualization/max_classD.png', bbox_inches='tight')
 plt.close(fig)
 
-print("Finished")
+model = torch.load('discriminator.model')
+model.cuda()
+model.eval()
+
+for i in xrange(200):
+    _, output = model(X)
+
+    loss = -output[torch.arange(10).type(torch.int64),torch.arange(10).type(torch.int64)]
+    gradients = torch.autograd.grad(outputs=loss, inputs=X,
+                              grad_outputs=torch.ones(loss.size()).cuda(),
+                              create_graph=True, retain_graph=False, only_inputs=True)[0]
+
+    prediction = output.data.max(1)[1] # first column has actual prob.
+    accuracy = ( float( prediction.eq(Y.data).sum() ) /float(10.0))*100.0
+    print(i,accuracy,-loss)
+
+    X = X - lr*gradients.data - weight_decay*X.data*torch.abs(X.data)
+    X[X>1.0] = 1.0
+    X[X<-1.0] = -1.0
+
+## save new images
+samples = X.data.cpu().numpy()
+samples += 1.0
+samples /= 2.0
+samples = samples.transpose(0,2,3,1)
+
+fig = plot(samples)
+plt.savefig('visualization/max_classDG.png', bbox_inches='tight')
+plt.close(fig)
