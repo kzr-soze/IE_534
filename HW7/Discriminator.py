6c5375c501aa1d42412381638df0caf4e7a32fff
Murray <tsmurra2@illinois.edu>
2019-11-13 14:01:02 -0600

    Starting HW7, part 2c.

--- HW7/Discriminator.py
+++ HW7/Discriminator.py
@@ -34,37 +34,37 @@ class Discriminator(nn.Module):
     def forward(self,x, extract_features=0):
         x = F.leaky_relu(self.ln1(self.conv1(x)))
         if(extract_features==1):
-            h = F.max_pool2d(x,4,4)
+            h = F.max_pool2d(x,32,32)
             h = h.view(-1, no_of_hidden_units)
             return self.fc1(h),h
         x = F.leaky_relu(self.ln2(self.conv2(x)))
         if(extract_features==2):
-            h = F.max_pool2d(x,4,4)
+            h = F.max_pool2d(x,16,16)
             h = h.view(-1, no_of_hidden_units)
             return self.fc1(h),h
         x = F.leaky_relu(self.ln3(self.conv3(x)))
         if(extract_features==3):
-            h = F.max_pool2d(x,4,4)
+            h = F.max_pool2d(x,16,16)
             h = h.view(-1, no_of_hidden_units)
             return self.fc1(h),h
         x = F.leaky_relu(self.ln4(self.conv4(x)))
         if(extract_features==4):
-            h = F.max_pool2d(x,4,4)
+            h = F.max_pool2d(x,8,8)
             h = h.view(-1, no_of_hidden_units)
             return self.fc1(h),h
         x = F.leaky_relu(self.ln5(self.conv5(x)))
         if(extract_features==5):
-            h = F.max_pool2d(x,4,4)
+            h = F.max_pool2d(x,8,8)
             h = h.view(-1, no_of_hidden_units)
             return self.fc1(h),h
         x = F.leaky_relu(self.ln6(self.conv6(x)))
         if(extract_features==6):
-            h = F.max_pool2d(x,4,4)
+            h = F.max_pool2d(x,8,8)
             h = h.view(-1, no_of_hidden_units)
             return self.fc1(h),h
         x = F.leaky_relu(self.ln7(self.conv7(x)))
         if(extract_features==7):
-            h = F.max_pool2d(x,4,4)
+            h = F.max_pool2d(x,8,8)
             h = h.view(-1, no_of_hidden_units)
             return self.fc1(h),h
         x = F.leaky_relu(self.ln8(self.conv8(x)))
