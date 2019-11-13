8ffed96010fa3471d5488aa0b839ef1c9c3b7742
Murray <tsmurra2@illinois.edu>
2019-11-13 14:52:08 -0600

    Starting HW7, part 2c.

--- HW7/2c.py
+++ HW7/2c.py
@@ -34,17 +34,17 @@ def plot(samples):
 
 batch_size = 100
 
-transform_train = transforms.Compose([
-    transforms.RandomResizedCrop(32, scale=(0.7, 1.0), ratio=(1.0,1.0)),
-    transforms.ColorJitter(
-            brightness=0.1*torch.randn(1),
-            contrast=0.1*torch.randn(1),
-            saturation=0.1*torch.randn(1),
-            hue=0.1*torch.randn(1)),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-])
+# transform_train = transforms.Compose([
+#     transforms.RandomResizedCrop(32, scale=(0.7, 1.0), ratio=(1.0,1.0)),
+#     transforms.ColorJitter(
+#             brightness=0.1*torch.randn(1),
+#             contrast=0.1*torch.randn(1),
+#             saturation=0.1*torch.randn(1),
+#             hue=0.1*torch.randn(1)),
+#     transforms.RandomHorizontalFlip(),
+#     transforms.ToTensor(),
+#     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+# ])
 
 transform_test = transforms.Compose([
     transforms.CenterCrop(32),
