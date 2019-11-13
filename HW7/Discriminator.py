dbbd4bc7238c5c8627a8eabff279b45c8f730346
Murray <tsmurra2@illinois.edu>
2019-11-13 13:56:18 -0600

    Starting HW7, part 2c.

--- HW7/Discriminator.py
+++ HW7/Discriminator.py
@@ -36,42 +36,42 @@ class Discriminator(nn.Module):
         if(extract_features==1):
             h = F.max_pool2d(x,4,4)
             h = h.view(-1, no_of_hidden_units)
-            return h
+            return self.fc1(h),h
         x = F.leaky_relu(self.ln2(self.conv2(x)))
         if(extract_features==2):
             h = F.max_pool2d(x,4,4)
             h = h.view(-1, no_of_hidden_units)
-            return h
+            return self.fc1(h),h
         x = F.leaky_relu(self.ln3(self.conv3(x)))
         if(extract_features==3):
             h = F.max_pool2d(x,4,4)
             h = h.view(-1, no_of_hidden_units)
-            return h
+            return self.fc1(h),h
         x = F.leaky_relu(self.ln4(self.conv4(x)))
         if(extract_features==4):
             h = F.max_pool2d(x,4,4)
             h = h.view(-1, no_of_hidden_units)
-            return h
+            return self.fc1(h),h
         x = F.leaky_relu(self.ln5(self.conv5(x)))
         if(extract_features==5):
             h = F.max_pool2d(x,4,4)
             h = h.view(-1, no_of_hidden_units)
-            return h
+            return self.fc1(h),h
         x = F.leaky_relu(self.ln6(self.conv6(x)))
         if(extract_features==6):
             h = F.max_pool2d(x,4,4)
             h = h.view(-1, no_of_hidden_units)
-            return h
+            return self.fc1(h),h
         x = F.leaky_relu(self.ln7(self.conv7(x)))
         if(extract_features==7):
             h = F.max_pool2d(x,4,4)
             h = h.view(-1, no_of_hidden_units)
-            return h
+            return self.fc1(h),h
         x = F.leaky_relu(self.ln8(self.conv8(x)))
         if(extract_features==8):
             h = F.max_pool2d(x,4,4)
             h = h.view(-1, no_of_hidden_units)
-            return h
+            return self.fc1(h),h
         # print(x.size())
         x = self.pool(x)
         # print(x.size())
